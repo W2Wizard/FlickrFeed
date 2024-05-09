938866dc8755df5697b1fe9c6cac4f2e5e3bc0de
W2Wizard <main@w2wizard.dev>
2024-05-09 17:47:53 +0200

    Fonts, spinners, css ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Loading from '$lib/loading.svelte';
 
 	const { data } = $props();
 
@@ -16,26 +17,57 @@
 	}
 </script>
 
-<form
-	role="search"
-	onsubmit={(e) => {
-		e.preventDefault(); // Svelte 5 removed piplines (workaround is there tho)
-		search(e.currentTarget.search.value);
-	}}
->
-	<input type="search" name="search" placeholder="Search..." />
-	<button type="submit">Search</button>
-</form>
+<header>
+	<form
+		role="search"
+		onsubmit={(e) => {
+			e.preventDefault(); // Svelte 5 removed piplines (workaround is there tho)
+			search(e.currentTarget.search.value);
+		}}
+	>
+		<input class="wui" type="text" spellcheck="false" name="search" placeholder="Search..." />
+		<button class="wui button" type="submit"> Search </button>
+	</form>
+</header>
 
-{#await data.streamed}
-	<p>...waiting</p>
-{:then feed}
-	{#each feed.items as item}
-		<article>
-			<h2>{item.title}</h2>
-			{@html item.description}
-		</article>
-	{/each}
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<main>
+	<ul class="graph">
+		{#await data.streamed}
+			<Loading />
+		{:then feed}
+			{#each feed.items as item}
+				<li>
+					<article>
+						<h2>{item.title}</h2>
+						{@html item.description}
+					</article>
+				</li>
+			{/each}
+		{:catch error}
+			<p>{error.message}</p>
+		{/await}
+	</ul>
+</main>
+
+<style>
+	header {
+		display: grid;
+		background-color: var(--wui-header);
+		width: 100%;
+		height: var(--wui-header-height);
+		padding-inline: 1rem;
+	}
+
+	main {
+		--main-padding: 1rem;
+		padding-inline: var(--main-padding);
+	}
+
+	.graph {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		border-radius: var(--wui-radius);
+		min-height: calc(100dvh - var(--main-padding) - var(--wui-header-height));
+		box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 4px 2px inset;
+	}
+</style>
